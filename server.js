ce91eac74302cac29c8a6c355b50ab53ae29cb54
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-11-17 10:38:03 +0100

    finished server file

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-express = require("express")
+const express = require("express")
 const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
 
 app.use(cors, morgan("dev"), bodyParser);
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+const apiRouter = express.Router();
+
+// Path: envelopes.js
+
