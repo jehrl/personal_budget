49dd2880d5ebb4e9551f3592f802031f4d73173d
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-11-20 18:13:43 +0100

    making logic of getting values

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 
-const envelopes = {
+let envelopes = {
     housing: {
-      rentMortgage: [],
+      rentOrMortgage: [{id: 1, name: "rent", amount: 1000, date: "2020-01-01", venue: "Venmo", notes: ""}, ],
       utilities: [],
       homeMaintenance: [],
     },
@@ -58,3 +58,50 @@ const envelopes = {
       donations: [],
     },
 };
+
+const getCategory = (name) => {
+    return envelopes[name];
+}
+
+const getSubcategory = (name, subcategory) => {
+    return envelopes[name][subcategory];
+}
+
+const getAllCategories = () => {
+    return envelopes;
+}
+
+const getRecordsByValue = (info, value, filter, nameOfFilter) => {
+    let records = [];
+    if (filter === "category") {
+        for (let subcategory in envelopes[nameOfFilter]) {
+            for (let record of envelopes[category][subcategory]) {
+                if (record[info] === value) {
+                    records.push(record);
+                }
+            }
+        }
+        return records;
+    }
+    if (filter === "subcategory") {
+        for (let record of envelopes[nameOfFilter]) {
+            if (record[info] === value) {
+                records.push(record);
+            }
+        }
+        return records;
+    } for (let category in envelopes) {
+        for (let subcategory in envelopes[category]) {
+            for (let record of envelopes[category][subcategory]) {
+                if (record[info] === value) {
+                    records.push(record);
+                }
+            }
+        }
+    }
+    return records;
+}
+
+
+
+console.log(getCategory("housing"))
