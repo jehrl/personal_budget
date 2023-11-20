b69c22f04f4083eca34b119b2b04560ed00008a2
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-11-20 17:04:08 +0100

    created envelopes for the database

---
+++ server/db.js
@@ -0,0 +1,60 @@
+
+const envelopes = {
+    housing: {
+      rentMortgage: [],
+      utilities: [],
+      homeMaintenance: [],
+    },
+    transportation: {
+      carPayment: [],
+      gas: [],
+      maintenanceRepairs: [],
+      publicTransportation: [],
+    },
+    groceries: {
+      food: [],
+      toiletries: [],
+      cleaningSupplies: [],
+    },
+    insurance: {
+      health: [],
+      car: [],
+      life: [],
+    },
+    debtPayments: {
+      creditCard1: [],
+      creditCard2: [],
+      studentLoans: [],
+    },
+    personalCare: {
+      haircuts: [],
+      toiletries: [],
+      cosmetics: [],
+    },
+    entertainment: {
+      diningOut: [],
+      moviesEntertainment: [],
+      hobbies: [],
+    },
+    savings: {
+      emergencyFund: [],
+      retirement: [],
+      generalSavings: [],
+    },
+    clothing: {
+      clothingPurchases: [],
+      dryCleaning: [],
+    },
+    education: {
+      tuition: [],
+      booksSupplies: [],
+    },
+    health: {
+      doctorVisits: [],
+      medications: [],
+    },
+    miscellaneous: {
+      gifts: [],
+      donations: [],
+    },
+};
